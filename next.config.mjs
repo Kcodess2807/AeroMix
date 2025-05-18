48d201b0c0a609d15f994234adc1502706b015e8
Karush2807 <arushkarnatak1881@gmail.com>
2025-05-19 01:09:36 +0530

    did domehting, added some components, backend setup

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,17 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+
+  async rewrites(){
+    return[
+      {
+        source: '/api/:path', 
+        destination: 'https://127.0.0.1: 5000/api/:path*', //route on which python api is running
+      },
+    ];
+  },
+
 }
 
 export default nextConfig
